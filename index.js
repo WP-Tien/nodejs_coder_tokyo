ffdd653723059bde3e1bbb6688c5a040a1adf411
nguyenhuutien3895 <nguyenhuutien.it.3895@gmail.com>
2019-12-06 22:50:07 +0700

    upgrade

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-console.log( process.env );
+require('dotenv').config();
+
+console.log( process.env.SESSION_SECRET );
+
 const express       = require('express');
 const bodyParser    = require('body-parser');
 const cookieParser  = require('cookie-parser');
@@ -16,7 +19,7 @@ app.set( 'view engine', 'pug' );
 
 app.use( bodyParser.json() ) // for parsing application/json
 app.use( bodyParser.urlencoded({ extended: true }) ) // for parsing application/x-www-form-urlencoded
-app.use( cookieParser('jfhdkjshfdsdasdqwecxz') );
+app.use( cookieParser( process.env.SESSION_SECRET ) );
 app.use( express.static('public') );
 
 app.use( '/users', authMiddleware.requireAuth, userRoutes );
